d72db1d0d4104ce0e30af0f932c371f3c7bc7065
Mike Owens <mike@filespanker.com>
2013-10-20 07:57:19 -0400

    Flatten scope, will allow multiple has_short_name configurations in the future.

--- lib/has_short_name.rb
+++ lib/has_short_name.rb
@@ -70,123 +70,116 @@ module HasShortName
 
   module ClassMethods
     def has_short_name(only: nil)
-      before_validation :assign_short_name
-      @short_name_options ||= {}
-      @short_name_options[:only] = only.nil? ? (->(m) { true }) : only
-    end
+      options = { only: only.nil? ? (->(m) { true }) : only }
 
-    def adjust_short_names!(scope: nil)
-      scope ||= self.all
-      scope = scope.to_a
+      define_singleton_method(:adjust_short_names!) do |scope: nil|
+        scope ||= self.all
+        scope = scope.to_a
 
-      # Our main structure here is:
-      # { 'Mike' =>  [[user1, candidates1],
-      #               [user2, candidates2]] }
-      name_map = scope.map do |u|
-        [u, u.short_name_candidates]
-      end.group_by do |r|
-        r.last.first
-      end
+        # Our main structure here is:
+        # { 'Mike' =>  [[user1, candidates1],
+        #               [user2, candidates2]] }
+        name_map = scope.map do |u|
+          [u, u.short_name_candidates]
+        end.group_by do |r|
+          r.last.first
+        end
 
-      loop do
-        adj_map = {}
-        name_map.each do |k, v|
-          if v.size == 1
-            adj_map[k] = v
-          else
-            resolve_conflicts(k, v) do |new_key, urec|
-              adj_map[new_key] ||= []
-              adj_map[new_key].push(urec)
+        loop do
+          adj_map = {}
+          name_map.each do |k, v|
+            if v.size == 1
+              adj_map[k] = v
+            else
+              resolve_conflicts(k, v) do |new_key, urec|
+                adj_map[new_key] ||= []
+                adj_map[new_key].push(urec)
+              end
             end
           end
+          name_map = adj_map
+
+          # We're done if each entry is singular, OR unsolvable.
+          done = name_map.all? do |k, v|
+            v.size == 1 || v.all? {|u, candidates| candidates.size == 1}
+          end
+
+          break if done
         end
-        name_map = adj_map
 
-        # We're done if each entry is singular, OR unsolvable.
-        done = name_map.all? do |k, v|
-          v.size == 1 || v.all? {|u, candidates| candidates.size == 1}
-        end
-
-        break if done
-      end
-
-      # Here, name_map should look something like:
-      # name_map = {'Mike' => [[User(...), [leftover candidates]]]}
-      name_map.each do |k, urecs|
-        urecs.each do |urec|
-          user = urec.first
-          user.update(short_name: k) if user.short_name != k
-        end
-      end
-    end
-
-    private
-    def resolve_conflicts(k, urecs)
-      urecs.each do |user, candidates|
-        fail "empty candidate list" if candidates.empty?
-        fail "conflicted key not first" if candidates.first != k
-        if candidates.size == 1
-          yield [candidates.first, [user, [candidates.first]]]
-        else
-          yield [candidates[1], [user, candidates[1..-1]]]
+        # Here, name_map should look something like:
+        # name_map = {'Mike' => [[User(...), [leftover candidates]]]}
+        name_map.each do |k, urecs|
+          urecs.each do |urec|
+            user = urec.first
+            user.update(short_name: k) if user.short_name != k
+          end
         end
       end
-    end
-  end
 
-  module Methods
-    def short_name_candidates
-      # For models that fail the predicate, the name is the only
-      # candidate.
-      options = self.class.instance_variable_get(:@short_name_options)
-      return [name] if !options[:only].(self)
-
-      # The rules should be in priority order.  As we match,
-      # we keep track of what's already matched and let further
-      # rules opt out if they want.
-      already_matched = []
-      after_rules = HasShortName.rules.map do |k, r|
-        candidate = (r.arity == 2 ? r.(name, already_matched) : r.(name))
-        already_matched.push(k) if candidate
-        candidate
-      end
-
-      after_rules.compact!
-      after_rules.map! {|v| v.gsub(/\s+/, ' ')}
-      after_rules.uniq!
-      after_rules
-    end
-
-    private
-    def assign_short_name
-      if !((new_record? && short_name.blank?) ||
-           (! new_record? && name_changed? && !short_name_changed?))
-        return
-      end
-
-      scope = self.class.all
-      short_name_candidates.each do |candidate|
-        if (ex = scope.find_by(short_name: candidate)) && ex != self
-          next
-        else
-          self.short_name = candidate
-          break
+      define_singleton_method(:resolve_conflicts) do |k, urecs, &cb|
+        urecs.each do |user, candidates|
+          fail "empty candidate list" if candidates.empty?
+          fail "conflicted key not first" if candidates.first != k
+          if candidates.size == 1
+            cb.([candidates.first, [user, [candidates.first]]])
+          else
+            cb.([candidates[1], [user, candidates[1..-1]]])
+          end
         end
       end
-    end
-  end
 
-  module ARHook
-    def has_short_name(*args, **kw)
-      include(HasShortName) unless is_a?(HasShortName)
-      has_short_name(*args, **kw)
+
+      define_method(:short_name_candidates) do
+        # For models that fail the predicate, the name is the only
+        # candidate.
+        return [name] if !options[:only].(self)
+
+        # The rules should be in priority order.  As we match,
+        # we keep track of what's already matched and let further
+        # rules opt out if they want.
+        already_matched = []
+        after_rules = HasShortName.rules.map do |k, r|
+          candidate = (r.arity == 2 ? r.(name, already_matched) : r.(name))
+          already_matched.push(k) if candidate
+          candidate
+        end
+
+        after_rules.compact!
+        after_rules.map! {|v| v.gsub(/\s+/, ' ')}
+        after_rules.uniq!
+        after_rules
+      end
+
+      define_method(:assign_short_name) do
+        if !((new_record? && short_name.blank?) ||
+             (! new_record? && name_changed? && !short_name_changed?))
+          return
+        end
+
+        scope = self.class.all
+        short_name_candidates.each do |candidate|
+          if (ex = scope.find_by(short_name: candidate)) && ex != self
+            next
+          else
+            self.short_name = candidate
+            break
+          end
+        end
+      end
+
+      before_validation :assign_short_name
     end
   end
 
   def self.included(cls)
-    cls.send(:include, Methods)
     cls.send(:extend,  ClassMethods)
   end
 end
 
-::ActiveRecord::Base.send(:extend, HasShortName::ARHook)
+class << ActiveRecord::Base
+  def has_short_name(*args, **kw)
+    include HasShortName
+    has_short_name(*args, **kw)
+  end
+end
--- test/has_short_name_test.rb
+++ test/has_short_name_test.rb
@@ -54,13 +54,11 @@ class HasShortNameTest < MiniTest::Unit::TestCase
   def test_hook_was_included
     # On AR models which has_short_name wasn't called, we shouldn't
     # have the ClassMethods mixins
-    assert_kind_of HasShortName::ARHook, NoMixin
     refute_kind_of HasShortName::ClassMethods, NoMixin
   end
 
   def test_bootstrap_handoff
     # It should still have the bootstrap
-    assert_kind_of HasShortName::ARHook, WithMixin
     assert_kind_of HasShortName::ClassMethods, WithMixin
   end
 
